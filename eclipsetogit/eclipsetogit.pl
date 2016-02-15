3a6fa4ac76689a865374e54d11981787ecea49d4
Heather Theinert <heather39@mail.usf.edu>
2016-02-14 21:03:04 -0500

    Second commit of git homework assignment.

--- eclipsetogit/eclipsetogit.pl
+++ eclipsetogit/eclipsetogit.pl
@@ -1,3 +1,6 @@
 #!/usr/bin/perl
 
-print "Heather Theinert"
+print "Heather Theinert";
+
+print "This perl project is being used as a test.\n";
+print "This project is helping me understand how to use Git in Eclipse."
